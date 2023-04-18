b018b2c05c64c1da1544051c3eb49ca1a29e4bdd
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-18 18:30:15 +0200

    Update

--- src/den03/Priklad03Odzadu.java
+++ src/den03/Priklad03Odzadu.java
@@ -7,11 +7,20 @@ public class Priklad03Odzadu {
 	 * Program vypíše zadané slovo odzadu.
 	 * 
 	 * Príklad:
-	 * Vstup: slovo
+	 * Vstup:  slovo
 	 * výstup: ovols
+	 * 
+	 * Pomôcky:
+	 * 	length - počet znakov v slove
+	 *  charAt - získa znak na danej pozícii
 	 */
 	public static void main(String[] args) {
 		String slovo = "slovo";
+		
+		for (int i = 0; i < slovo.length(); i++) {
+			System.out.print( slovo.charAt(slovo.length() - 1 - i) );			
+		}
+		
 	}
 
 }
