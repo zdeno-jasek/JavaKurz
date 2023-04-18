82af35811b579ab82725365bf1f80ec0331755c6
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-18 18:44:26 +0200

    Update

--- src/den03/Priklad05IndexOf.java
+++ src/den03/Priklad05IndexOf.java
@@ -13,7 +13,12 @@ public class Priklad05IndexOf {
 		int poziciaPismenaF = slovo.indexOf( 'f' );
 		System.out.println( "Pozicia pismena f je: " + poziciaPismenaF );
 		// TODO: vypísať písmeno na pozícii "poziciaPismenaF"
+		System.out.println(slovo.charAt(poziciaPismenaF));
 		// TODO: vypísať pozíciu písmena "x" v slove "Kufor"
+		int poziciaX = slovo.indexOf( 'x' );
+		System.out.println( poziciaX);
 		// TODO: vypísať pozíciu písmena "F" v slove "Kufor"
+		int poziciaF = slovo.indexOf( 'F' );
+		System.out.println( poziciaF);
 	}
 }
