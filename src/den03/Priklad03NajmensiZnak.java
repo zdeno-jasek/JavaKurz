c8612b300c2493ee6c6666c0e60d082dec2973bd
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-18 19:02:22 +0200

    Update

--- src/den03/Priklad03NajmensiZnak.java
+++ src/den03/Priklad03NajmensiZnak.java
@@ -7,9 +7,20 @@ public class Priklad03NajmensiZnak {
 	 * Program vypíše najmenší znak v texte, t.j. znak, ktorý je abecedne prvý v poradí. 
 	 * Vstup: "pokus" 
 	 * Výstup: "k"
+	 * 
+	 * Vstup: "dcbaxyz" 
+	 * Výstup: "a"
+	 * 
 	 */
 	public static void main(String[] args) {
-		String text = "pokus";
+		String text = "dcbaxyz";
 
+		char najmensiePismeno = text.charAt(0);
+		for (int i = 1; i < text.length(); i++) {
+			if ( najmensiePismeno > text.charAt(i) ) {
+				najmensiePismeno = text.charAt(i);
+			}
+		}
+		System.out.println( "Najmensie pismeno v slove " + text + " je " + najmensiePismeno );
 	}
 }
