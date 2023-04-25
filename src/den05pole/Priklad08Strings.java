ef3ea055cbbad8b15d511ba1c83da60c9b0fe61e
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-25 15:53:05 +0200

    Update

---
+++ src/den05pole/Priklad08Strings.java
@@ -0,0 +1,15 @@
+package den05pole;
+
+public class Priklad08Strings {
+
+	/*
+	 * String má metódu split, ktorá rozdelí String podľa nejakého znaku, alebo skupiny znakov.
+	 * Výsledkom je pole String-ov. 
+	 */
+	public static void main(String[] args) {
+		String veta = "Toto je nejaka dlha veta";
+		String[] slova = veta.split( " " );
+		
+		// TODO vypisat slova
+	}
+}
